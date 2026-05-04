483ba00bd57c08fb279879ddc0e46a53069b9061
osager-net <RuslanGamidov2015@yandex.ru>
2026-05-04 13:45:31 +0400

    Update wincontrol.bat

--- wincontrol.bat
+++ wincontrol.bat
@@ -15,9 +15,9 @@ echo /taskmgr (Открытие диспетчера задач) (Обычная
 echo /exit (Закрытие программы управления) (Может быть опасно при активности вируса) (Завершательная команда)
 
 set /p command=Введите команду: 
-if "%command%"=="/commands" (goto commands)
-if "%command%"=="/shutdown" (goto shutdown)
-if "%command%"=="/enablemgr" (goto enablemgr)
+if "!command!"=="/commands" (goto commands)
+if "!command!"=="/shutdown" (goto shutdown)
+if "!command!"=="/enablemgr" (goto enablemgr)
 if "%command%"=="/taskmgr" (goto taskmgr)
 if "%command%"=="/exit" (goto exit)
 else (goto noncmd)
